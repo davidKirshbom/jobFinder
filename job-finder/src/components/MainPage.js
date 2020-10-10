93949aae2e90fab194ba1f8d19f49e1651865588
david kirshbom <davidkirshbomloy@gmail.com>
2020-10-10 17:43:34 +0300

    added navigation boxes

--- job-finder/src/components/MainPage.js
+++ job-finder/src/components/MainPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavigationBar from './NavigationBar'
 import SearchPanel from './SearchPanel'
+import NavigationBoxes from './NavigationBoxs'
 import '../styles/sheared.scss'
 export default () => 
     (<div className="page-container">
         <NavigationBar />
         <section>
             <SearchPanel />
-            
         </section>
+        <NavigationBoxes/>
+
     </div>)
---
+++ job-finder/src/components/NavigationBoxs.js
@@ -0,0 +1,86 @@
+import React,{useEffect} from 'react'
+
+export default () => {
+    useEffect(() => {
+        const salaryChartBox = document.querySelector(".box.salary-tabs-box");
+        salaryChartBox.addEventListener("click", () => {
+            if (salaryChartBox.classList.contains("expended"))
+                salaryChartBox.classList.remove("expended")
+            else
+                salaryChartBox.classList.add("expended")
+        })
+    },[])
+    return (<div className="boxes-container">
+    <div className="box candidate-box">
+        <div className="box-header">
+                <div className="text-container">
+                    מידע למועמדים
+                    <i class="fas fa-plus"></i>
+                </div>
+        </div>
+            <div className="box-contant">
+                <ul>
+                    <li>חברות הייטק</li>
+                    <li>מגמות שוק</li>
+                    <li>פודקאסטים</li>
+                    <li>פורום קריירה</li>
+                    <li>פורום דיני עבודה</li>
+                </ul>
+        </div>
+    </div>
+    <div className="box senior-box">
+        <div className="box-header">
+                <div className="text-container">
+                    בכירים
+                    <i class="fas fa-plus"></i>
+                </div>
+        </div>
+            <div className="box-contant">
+                <ul>
+                    <li>משרות בכירות</li>
+                    <li>אתר בכירים</li>
+                    <li>פניה אישית ודיסקרטית</li>
+                </ul>
+        </div>
+    </div>
+    <div className="box salary-tabs-box">
+        <div className="box-header">
+                <div className="text-container">
+                    טבלאות שכר 
+                    <i class="fas fa-plus"></i> 
+                    <i id="minus-font" class="fas fa-minus"></i>
+                </div>
+        </div>
+            <div className="box-contant">
+                <ul>
+                    <li>תוכנה</li>
+                    <li>אינטרנט</li>
+                    <li>חומרה</li>
+                    <li>ביומד</li>
+                    <li>שיווק ומכירות</li>
+                    <li>משאבי אנוש</li>
+                    <li>בכירים</li>
+                    <li>הייטק</li>
+                </ul>
+        </div>
+    </div>
+    <div className="box personal-box">
+        <div className="box-header">
+                <div className="text-container">
+                    פנייה אישית
+                    <i class="fas fa-plus"></i>
+                </div>
+        </div>
+        <div className="box-contant">
+                <ul>
+                    <li>מתכנתים ומהנדסי תוכנה</li>
+                    <li>מהנדסי חומרה ומכונות</li>
+                    <li>יוצאי יחידות טכנולוגיות</li>
+                    <li>דרושים הייטק</li>
+                    <li>ביוטק ביומד קלינטק</li>
+                </ul>
+            </div>
+        </div>
+        <div className="box linkedin-box"></div>
+    </div>)
+}
